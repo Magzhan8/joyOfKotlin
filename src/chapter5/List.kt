08bceb12da556bf9be36311962a8672c133af65d
zhaksykeldi_46437 <Magzhan.Zhaksykeldi@kaspi.kz>
2020-05-14 10:41:43 +0600

    additional functions

--- src/chapter5/List.kt
+++ src/chapter5/List.kt
@@ -4,7 +4,7 @@ sealed class List<A> {
 
     fun cons(a: A): List<A> = Cons(a, this)
 
-    fun setHead(a: A): List<A> = when(this) {
+    fun setHead(a: A): List<A> = when (this) {
         Nil -> throw IllegalStateException("setHead called on an empty list")
         is Cons -> tail.cons(a)
     }
@@ -13,35 +13,40 @@ sealed class List<A> {
 
     fun dropWhile(p: (A) -> Boolean): List<A> = dropWhile(this, p)
 
+    fun init(): List<A> = reverse().drop(1).reverse()
+
+    fun reverse(): List<A> = reverse(List.invoke(), this)
+
     abstract fun isEmpty(): Boolean
 
-    private object Nil: List<Nothing>() {
+    private object Nil : List<Nothing>() {
 
         override fun isEmpty(): Boolean = true
 
         override fun toString(): String = "[NIL]"
     }
 
-    private class Cons<A>(
-            internal val head: A,
-            internal val tail: List<A>): List<A>() {
+    internal class Cons<A>(
+        val head: A,
+        val tail: List<A>
+    ) : List<A>() {
 
         override fun isEmpty(): Boolean = false
 
         override fun toString(): String = "[${toString("", this)}NIL]"
 
         tailrec fun toString(acc: String, list: List<A>): String =
-                when(list) {
-                    is Nil -> acc
-                    is Cons -> toString("$acc${list.head}", list.tail)
-                }
+            when (list) {
+                is Nil -> acc
+                is Cons -> toString("$acc${list.head}", list.tail)
+            }
     }
 
     companion object {
 
         operator
-        fun <A> invoke(vararg az:A): List<A> =
-                az.foldRight(Nil as List<A>) { a: A, list: List<A> -> Cons(a, list)}
+        fun <A> invoke(vararg az: A): List<A> =
+            az.foldRight(Nil as List<A>) { a: A, list: List<A> -> Cons(a, list) }
 
         tailrec fun <A> drop(list: List<A>, n: Int): List<A> = when (list) {
             List.Nil -> list
@@ -49,10 +54,16 @@ sealed class List<A> {
         }
 
         tailrec fun <A> dropWhile(list: List<A>, p: (A) -> Boolean): List<A> =
-                when(list) {
-                    Nil -> list
-                    is Cons -> if(p(list.head)) dropWhile(list.tail, p) else list
-                }
+            when (list) {
+                Nil -> list
+                is Cons -> if (p(list.head)) dropWhile(list.tail, p) else list
+            }
+
+        tailrec fun <A> reverse(acc: List<A>, list: List<A>): List<A> =
+            when(list) {
+                is Nil -> acc
+                is Cons -> reverse(acc.cons(list.head), list.tail)
+            }
     }
 
 }
